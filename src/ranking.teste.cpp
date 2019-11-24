78aef10089f7e65e6c4cfbafb78b707ecf479d8c
mmarchesotti <49643052+mmarchesotti@users.noreply.github.com>
2019-11-24 01:26:07 -0300

    Add files via upload

--- src/ranking.teste.cpp
+++ src/ranking.teste.cpp
@@ -1,5 +1,8 @@
 #include "doctest.h"
 
+#define endereco "../documentos/testes/"
+#define arquivo  "text.txt"
+
 class Teste{
 	public:
 		static Dataset* ds(const Ranking &r){
@@ -15,29 +18,37 @@ class Teste{
 
 TEST_SUITE("Ranking"){
 	TEST_CASE("Ranking()"){
-		Dataset ds_test;
-		Ranking r(ds_test, "test.txt");
+		Dataset ds_test(endereco);		
+		Indice_invertido id_iv_test(&ds_test, arquivo);
+		Ranking r(id_iv_teste, &ds_test);
 		CHECK(Teste::ds(r) == &ds_test);
-		CHECK(Teste::ds(r) == "test.txt");
+		CHECK(Teste::sum_com_2(r) == 0);
+		CHECK(Teste::arq_coord(r) == {0,0,0})
 	}
 
 	TESTE_CASE("Ler_coordenadas()"){
-
+		Dataset ds_test(endereco);		
+		Indice_invertido id_iv_test(&ds_test, arquivo);
+		Ranking r(id_iv_teste, &ds_test);
+		CHECK(ds_test.Ler_coordenadas(1) == {0,0,0});
+		CHECK(ds_test.Ler_coordenadas(2) == {0,0,0});
 	}
 
 	TESTE_CASE("Similaridade()"){
-
+		Dataset ds_test(endereco);		
+		Indice_invertido id_iv_test(&ds_test, arquivo);
+		Ranking r(id_iv_teste, &ds_test);
+		CHECK(ds_test.Similaridade(1) == 0);
+		CHECK(ds_test.Similaridade(2) == 0);
 	}
 
 	TESTE_CASE("Ordenar_documentos()"){
-
-	}
-
-	TESTE_CASE("Mostrar_ranking()"){
-
-	}
-
-	TESTE_CASE("~Ranking()"){
-
+		Dataset ds_test(endereco);		
+		Indice_invertido id_iv_test(&ds_test, arquivo);
+		Ranking r(id_iv_teste, &ds_test);
+		vector<vector<int> > v_teste;
+		v_teste[0].push_back(0);
+		v_teste[0].push_back(1);
+		CHECK(ds_test.Ordenar_documentos() == v_teste);
 	}
 }
